cd0f1d7f9bcd22d85f0be7283ddd354523f345c2
ArtinaBektashi <ab51034@ubt-uni.net>
2023-05-01 00:22:14 +0200

    rooms crud operations

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,22 @@
 import express from "express";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+//create
+router.post("/:hotelid", createRoom);
+
+//update
+router.put("/:id", updateRoom);
+
+//delete
+router.delete("/:id/:hotelid", deleteRoom);
+
+//get by id
+router.get("/:id", getRoom);
+
+//Get
+router.get("/", getRooms);
 
 export default router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -25,6 +25,7 @@ const router = express.Router();
 router.put("/:id", verifyUser, updateUser);
 
 //delete
+router.delete("/:id",verifyUser,deleteUser);
 router.delete("/:id", deleteUser);
 
 //get by id
