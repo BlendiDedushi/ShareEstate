289bbe3c0b30eb62a8f55ab2eb0edd6d29ce919f
ArtinaBektashi <ab51034@ubt-uni.net>
2023-05-08 10:18:02 +0200

    bug fixes

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -23,7 +23,7 @@ export const register = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
   try {
-    const user = await User.findOne({ username: req.body.username });
+    const user = await User.findOne({ where: { username: req.body.username } });
     if (!user) return next(createError(404, "User not found!"));
 
     const isPasswordCorrect = await bcrypt.compare(
@@ -34,18 +34,47 @@ export const login = async (req, res, next) => {
       return next(createError(400, "Wrong password!"));
 
     const token = jwt.sign(
-      { id: user._id, isAdmin: user.isAdmin },
+      { id: user.id, isAdmin: user.isAdmin },
       process.env.JWT
     );
 
-    const { password, isAdmin, ...otherDetails } = user._doc;
+    // Remove the destructuring assignment and directly include the desired properties in the response
     res
       .cookie("access_token", token, {
         httpOnly: true,
       })
       .status(200)
-      .json({ ...otherDetails });
+      .json({ id: user.id, isAdmin: user.isAdmin });
   } catch (err) {
     next(err);
   }
 };
+
+// export const login = async (req, res, next) => {
+//   try {
+//     const user = await User.findOne({ where: { username: req.body.username } });
+//     if (!user) return next(createError(404, "User not found!"));
+
+//     const isPasswordCorrect = await bcrypt.compare(
+//       req.body.password,
+//       user.password
+//     );
+//     if (!isPasswordCorrect)
+//       return next(createError(400, "Wrong password!"));
+
+//     const token = jwt.sign(
+//       { id: user._id, isAdmin: user.isAdmin },
+//       process.env.JWT
+//     );
+
+//     const { password, isAdmin, ...otherDetails } = user._doc;
+//     res
+//       .cookie("access_token", token, {
+//         httpOnly: true,
+//       })
+//       .status(200)
+//       .json({ ...otherDetails });
+//   } catch (err) {
+//     next(err);
+//   }
+// };
