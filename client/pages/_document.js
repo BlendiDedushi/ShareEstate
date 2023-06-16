2616c0ca604f18d1d43c3734f39dfaa82a7dc580
Ledejna <ledejnasalihu4@gmail.com>
2023-06-16 07:02:52 +0200

    chat integration

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -3,7 +3,24 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <Head>
+            <script
+                type="text/javascript"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                window.$crisp = [];
+                window.CRISP_WEBSITE_ID = "551fc8d5-1910-45eb-bb17-cea9321e7c8a";
+                (function () {
+                  var d = document;
+                  var s = d.createElement("script");
+                  s.src = "https://client.crisp.chat/l.js";
+                  s.async = 1;
+                  d.getElementsByTagName("head")[0].appendChild(s);
+                })();
+              `,
+                }}
+            />
+        </Head>
       <body>
         <Main />
         <NextScript />
